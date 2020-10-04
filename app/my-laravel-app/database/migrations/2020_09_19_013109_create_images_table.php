980d0ddd2e438af48bfef34cac0cb6c6afd6f397
shimamar <mari.shimada4@gmail.com>
2020-10-04 20:43:17 +0900

    postシーダー imageシーダー追加

--- app/my-laravel-app/database/migrations/2020_09_19_013109_create_images_table.php
+++ app/my-laravel-app/database/migrations/2020_09_19_013109_create_images_table.php
@@ -16,7 +16,7 @@ class CreateImagesTable extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('post_id')->unsigned()->comment('投稿ID');
-            $table->string('icon')->unique();
+            $table->string('icon');
             $table->timestamps();
             $table->softDeletes();
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
---
+++ app/my-laravel-app/database/seeds/ImageSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Image;
+use App\Post;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts = Post::all();
+        $posts = $posts->shuffle();
+        $posts->all();
+        foreach ($posts as $post) {
+            factory(Image::class)
+            ->create([
+                'post_id' => $post->id
+            ]);
+        }
+    }
+}

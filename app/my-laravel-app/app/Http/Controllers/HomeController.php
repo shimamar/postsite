bd94cdb3d6dc385372460c932b545ea770ca0da5
shimamar <mari.shimada4@gmail.com>
2020-11-07 11:41:31 +0900

    詳細画面実装

--- app/my-laravel-app/app/Http/Controllers/HomeController.php
+++ app/my-laravel-app/app/Http/Controllers/HomeController.php
@@ -5,18 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 
-class HomeController extends Controller
+class HomeController extends BaseAuthController
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
